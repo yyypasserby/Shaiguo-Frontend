5af39a0634019860cbe0e93b81a8aaef69655f29
yyypasserby <yyypasserby@promote.cache-dns.local>
2014-09-03 00:50:03 +0800

    add user post

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the livesApp
  */
 angular.module('livesApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope, $resource) {
+    $scope.user={};
+    var url = 'http://localhost:8080/LivesServer/rest/:func/:method';
+    $scope.signup = function() {
+        console.log($scope.user);  
+        var userResource = $resource(url, {func : 'user', method : 'addUser'});
+        userResource.username = $scope.user.username;
+        userResource.password = $scope.user.password;
+        userResource.email    = $scope.user.email;
+
+        var result = userResource.save(function() {
+            console.log(result);
+        });
+    };
+});
