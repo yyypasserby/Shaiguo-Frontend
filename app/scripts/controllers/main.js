cfd8dd7be50700ed7ef026ff9a5909811811686a
yyypasserby <yyypasserby@yyypasserbydeMacBook-Pro.local>
2014-09-03 20:01:27 +0800

    modify index main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,15 +10,13 @@
 angular.module('livesApp')
 .controller('MainCtrl', function ($scope, $resource) {
     $scope.user={};
-    var url = 'http://localhost:8080/LivesServer/rest/:func/:method';
+    var url = 'http://localhost:8080/LivesServer/rest/:func';
     $scope.signup = function() {
         console.log($scope.user);  
-        var userResource = $resource(url, {func : 'user', method : 'addUser'});
-        userResource.username = $scope.user.username;
-        userResource.password = $scope.user.password;
-        userResource.email    = $scope.user.email;
+        var userResource = $resource(url, {func : 'user'});
+        console.log(userResource);
 
-        var result = userResource.save(function() {
+        var result = userResource.save($scope.user, function() {
             console.log(result);
         });
     };
