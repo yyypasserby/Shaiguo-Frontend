0cfd444e2d20b5ca994df41c349c19d97e37139c
sdlny <471153168@qq.com>
2014-09-04 15:26:50 +0800

    update personal,cast

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,6 +31,10 @@ var app = angular
         templateUrl: 'views/lives.html',
         controller: 'LivesCtrl'
       })
+      .when('/cast', {
+        templateUrl: 'views/cast.html',
+        controller: 'CastCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
