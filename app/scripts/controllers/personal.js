cb4e5b4444ac092aff2a79728137c18b689f38e9
yyypasserby <yyypasserby@yyypasserbydeMacBook-Pro.local>
2014-09-05 14:53:12 +0800

    add personal info friend, debug losing ui-bootstrap

--- app/scripts/controllers/personal.js
+++ app/scripts/controllers/personal.js
@@ -9,9 +9,20 @@
  */
 angular.module('livesApp')
 .controller('PersonalCtrl', function ($scope, $resource) {
-    var url = 'http://localhost:8080/LivesServer/rest/message';
-    var messageResource = $resource(url, {userId : 1});
+    var url = 'http://localhost:8080/LivesServer/rest/:func';
+    var messageResource = $resource(url, {func: 'message', userId : 0});
     $scope.messages = messageResource.query(function() {
         console.log($scope.messages);
     });
+    
+    var personResource = $resource(url, {func : 'user', userId : 0});
+    $scope.user = personResource.get(function() {
+        console.log($scope.user);
+    });
+
+    var friendResource = $resource(url, {func: 'friend', userId : 0});
+    $scope.friends = friendResource.query(function() {
+        console.log($scope.friend);
+    });
+
 });
